b139a8d60663c593f3386342597f08b740ceb84a
unknown <jamespaulmoriarty@gmail.com>
2020-06-11 18:28:49 +1000

    refinfment: cleanup formating.

--- gobat2exe.go
+++ gobat2exe.go
@@ -50,42 +50,42 @@ func Bat2Exe(batPath string) error {
 // SEDFileContents return string representation of Self Extraction Directive https://ss64.com/nt/iexpress-sed.html
 func SEDFileContents(batName string, batDirectoryPath string) string {
 	return `
-[Version]
-Class=IEXPRESS
-SEDVersion=3
-[Options]
-PackagePurpose=InstallApp
-ShowInstallProgramWindow=0
-HideExtractAnimation=1
-UseLongFileName=1
-InsideCompressed=0
-CAB_FixedSize=0
-CAB_ResvCodeSigning=0
-RebootMode=N
-InstallPrompt=%InstallPrompt%
-DisplayLicense=%DisplayLicense%
-FinishMessage=%FinishMessage%
-TargetName=%TargetName%
-FriendlyName=%FriendlyName%
-AppLaunched=%AppLaunched%
-PostInstallCmd=%PostInstallCmd%
-AdminQuietInstCmd=%AdminQuietInstCmd%
-UserQuietInstCmd=%UserQuietInstCmd%
-SourceFiles=SourceFiles
-[Strings]
-InstallPrompt=
-DisplayLicense=
-FinishMessage=
-TargetName=` + batDirectoryPath + `\` + batName + `.exe
-FriendlyName=` + batName + `
-AppLaunched=cmd /c ` + batName + `
-PostInstallCmd=<None>
-AdminQuietInstCmd=
-UserQuietInstCmd=%UserQuietInstCmd%
-FILE0="` + batName + `"
-[SourceFiles]
-SourceFiles0=` + batDirectoryPath + `
-[SourceFiles0]
-%FILE0%=
-`
+		[Version]
+		Class=IEXPRESS
+		SEDVersion=3
+		[Options]
+		PackagePurpose=InstallApp
+		ShowInstallProgramWindow=0
+		HideExtractAnimation=1
+		UseLongFileName=1
+		InsideCompressed=0
+		CAB_FixedSize=0
+		CAB_ResvCodeSigning=0
+		RebootMode=N
+		InstallPrompt=%InstallPrompt%
+		DisplayLicense=%DisplayLicense%
+		FinishMessage=%FinishMessage%
+		TargetName=%TargetName%
+		FriendlyName=%FriendlyName%
+		AppLaunched=%AppLaunched%
+		PostInstallCmd=%PostInstallCmd%
+		AdminQuietInstCmd=%AdminQuietInstCmd%
+		UserQuietInstCmd=%UserQuietInstCmd%
+		SourceFiles=SourceFiles
+		[Strings]
+		InstallPrompt=
+		DisplayLicense=
+		FinishMessage=
+		TargetName=` + batDirectoryPath + `\` + batName + `.exe
+		FriendlyName=` + batName + `
+		AppLaunched=cmd /c ` + batName + `
+		PostInstallCmd=<None>
+		AdminQuietInstCmd=
+		UserQuietInstCmd=%UserQuietInstCmd%
+		FILE0="` + batName + `"
+		[SourceFiles]
+		SourceFiles0=` + batDirectoryPath + `
+		[SourceFiles0]
+		%FILE0%=
+		`
 }
